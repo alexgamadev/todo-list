30ae2fa2f5b2bbe08c4a203931db1151cac54db1
AlexGamaDev <alexgama.97@outlook.com>
2020-11-10 23:31:53 +0000

    Adjusted some colours and added test code to open and close projects

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {init, loadHome} from "./modules/home";
 const appElement = document.getElementById("app");
 
 loadHeader(appElement);
-loadHome(appElement);
+//loadHome(appElement);
 
 
 /* ==================================================================
@@ -14,16 +14,26 @@ const project = document.getElementsByClassName("project-title")[0];
 
 project.addEventListener('click', (({currentTarget}) => {
     let classList = currentTarget.classList;
+    //Select
     if(classList.contains("unselected")) {
         classList.remove("unselected");
         classList.add("selected");
         currentTarget.children[0].classList.remove("fa-angle-right");
         currentTarget.children[0].classList.add("fa-angle-down");
-    } else if(classList.contains("selected")){
+        const listContainer = document.getElementsByClassName("list-container")[0];
+        listContainer.classList.remove("closed");
+        listContainer.classList.add("opened");
+        console.log(listContainer);
+    } 
+    //Unselect
+    else if(classList.contains("selected")) {
         classList.add("unselected");
         classList.remove("selected");
         currentTarget.children[0].classList.add("fa-angle-right");
         currentTarget.children[0].classList.remove("fa-angle-down");
+        const listContainer = document.getElementsByClassName("list-container")[0];
+        listContainer.classList.remove("opened");
+        listContainer.classList.add("closed");        
     }
 }));
 /* ================================================================== */
