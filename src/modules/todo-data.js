03b293e5be21e96fde98b59d60459ad9f6934b93
AlexGamaDev <alexgama.97@outlook.com>
2020-11-23 16:17:14 +0000

    Made checklist in TodoData a default parameter if one isn't entered, added priority variable to TodoData, added HTML code generation for priority traffic lights in JS

--- src/modules/todo-data.js
+++ src/modules/todo-data.js
@@ -1,13 +1,10 @@
 
 class TodoData {
-    constructor(title, notes, checklist){
+    constructor(title, notes, checklist = new Map()){
         this._title = title;
         this._notes = notes;
-        if(checklist) {
-            this._checklist = checklist;
-        } else {
-            this._checklist = new Map();
-        }
+        this._checklist = checklist;
+        this._priority = 0;
         
     };
 
@@ -18,6 +15,21 @@ class TodoData {
     get notes() {
         return this._notes;
     };
+
+    get priority() {
+        switch(this._priority) {
+            case 0: 
+                return "None";
+            case 1: 
+                return "Low";
+            case 2:
+                return "Medium";
+            case 3:
+                return "High";
+            default:
+                return "Invalid";
+        }
+    }
 
     get checklist() {
         return this._checklist;
@@ -30,6 +42,13 @@ class TodoData {
     set notes(notes) {
         this._notes = notes;
     };
+
+    set priority(priority) {
+        //Only alow priority between 0 and 3
+        if(priority >= 0 && priority <= 3) {
+            this._priority = priority;
+        }
+    }
 
     addChecklistItem(item, checked) {
         _checklist.set(item, checked);
